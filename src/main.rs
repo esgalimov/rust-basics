b29c947d64acb773ae31dd60dab340a56cf37f7b
esgalimov <esgalimov@gmail.com>
2025-09-15 22:12:33 +0300

    Errors processing with Result

--- src/main.rs
+++ src/main.rs
@@ -2,14 +2,33 @@
 mod calculator {
     type CalcType = i32;
 
-    #[derive(Default)]
-    pub struct Calc {
-        pub reg: CalcType,
+    #[derive(Debug)]
+    pub enum MathError {
+        DivisionByZero,
+        RegOverflow,
     }
 
     impl std::fmt::Display for Calc {
         fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-            write!(f, "reg = {}", self.reg)
+            match &self.reg {
+                Ok(value) =>  write!(f, "reg = {}", value),
+                Err(value) => {
+                    match value {
+                        MathError::DivisionByZero => write!(f, "Division by zero"),
+                        MathError::RegOverflow => write!(f, "Register overflow"),
+                    }
+                }
+            }
+        }
+    }
+
+    pub struct Calc {
+        pub reg: Result<CalcType, MathError>,
+    }
+
+    impl Default for Calc {
+        fn default() -> Self {
+            Self { reg: Ok(0) }
         }
     }
 
@@ -22,31 +41,70 @@ mod calculator {
 
     impl Math for Calc {
         fn add(mut self, rhs: CalcType) -> Self {
-            self.reg += rhs;
+            match self.reg {
+                Ok(value) => self.reg = value.checked_add(rhs).ok_or(MathError::RegOverflow),
+                Err(_) => {}
+            }
             self
         }
 
         fn sub(mut self, rhs: CalcType) -> Self {
-            self.reg -= rhs;
+            match self.reg {
+                Ok(value) => self.reg = value.checked_sub(rhs).ok_or(MathError::RegOverflow),
+                Err(_) => {}
+            }
             self
         }
 
         fn mul(mut self, rhs: CalcType) -> Self {
-            self.reg *= rhs;
+            match self.reg {
+                Ok(value) => self.reg = value.checked_mul(rhs).ok_or(MathError::RegOverflow),
+                Err(_) => {}
+            }
             self
         }
 
         fn div(mut self, rhs: CalcType) -> Self {
-            self.reg /= rhs;
+            if rhs == 0 {
+                self.reg = Err(MathError::DivisionByZero);
+            }
+            else {
+                self.reg = self.reg.map(|value| value / rhs);
+            }
             self
         }
     }
 }
 
 use crate::calculator::Math;
+use crate::calculator::Calc;
 
 fn main() {
-    let calc = calculator::Calc{reg: 1}.add(2).sub(1).mul(10).div(5);
+    let calc = Calc::default().add(2).sub(1).mul(10).div(1);
 
     println!("{calc}");
+}
+
+#[cfg(test)]
+mod test {
+    use crate::calculator::Math;
+    use crate::calculator::Calc;
+
+    #[test]
+    fn simple_addition() {
+        let res = Calc::default().add(2).add(2);
+        assert_eq!("reg = 4", res.to_string());
+    }
+
+    #[test]
+    fn overflow() {
+        let res = Calc::default().add(i32::MAX).add(1);
+        assert_eq!("Register overflow", res.to_string());
+    }
+
+    #[test]
+    fn division_by_zero() {
+        let res = Calc::default().div(0);
+        assert_eq!("Division by zero", res.to_string());
+    }
 }
